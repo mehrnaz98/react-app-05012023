43e0494762444cf26875ba8f22cb202b252618ce
mehrnaz98 <mehrnaz.eft98@gmail.com>
2023-05-01 15:45:45 +0430

    changed text

--- src/App.js
+++ src/App.js
@@ -7,15 +7,7 @@ export default function App() {
       <div className="container">
         <Weather defaultCity="New York" />
         <footer>
-          This project was coded by{" "}
-          <a
-            href="https://www.linkedin.com/in/antonina-dulama-682a2aa5/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Antonina Dulama
-          </a>{" "}
-          and is{" "}
+          This project was coded by Mehrnaz Eftekhari and is{" "}
           <a
             href="https://github.com/DulamaA/react-weather-app"
             target="_blank"
